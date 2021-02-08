4e361794d354b8a61608089b7fff520494b7999e
Nexu <alexandrunicolae.neacsu@campusfp.es>
2021-02-08 09:47:00 +0100

    Rama Principal

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Proyecto Ramas</title>
+    </head>
+    <body>
+        <form name="formulario" action="procesar.php" method="POST">
+            <label>Introduce tu nombre:</label>
+            <input type="text" name="nombre">
+            <label>Introduce la edad:</label>
+            <input type="text" name="edad">
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
+</html>
