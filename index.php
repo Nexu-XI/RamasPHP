9d32d0a4df64994439a52f8852ec9b6145db6345
Nexu <alexandrunicolae.neacsu@campusfp.es>
2021-02-08 10:47:49 +0100

    Operaciones completadas

--- index.php
+++ index.php
@@ -5,12 +5,51 @@
         <title>Proyecto Ramas</title>
     </head>
     <body>
-        <form name="formulario" action="procesar.php" method="POST">
+        <hr>
+        <form name="formulario_procesar" action="procesar.php" method="POST">
+            <h2>Procesar</h2>
             <label>Introduce tu nombre:</label>
             <input type="text" name="nombre">
             <label>Introduce la edad:</label>
             <input type="text" name="edad">
             <input type="submit" value="Enviar">
         </form>
+        <hr>
+        <form name="formulario_sumar" action="Suma.php" method="POST">
+            <h2>Sumar</h2>
+            <label>Numero 1</label>
+            <input type="number" name="num1">
+            <label>Numero 2</label>
+            <input type="number" name="num2">
+            <input type="submit" value="Enviar">
+        </form>
+        <hr>
+        <form name="formulario_restar" action="Resta.php" method="POST">
+            <h2>Restar</h2>
+            <label>Numero 1</label>
+            <input type="number" name="num1">
+            <label>Numero 2</label>
+            <input type="number" name="num2">
+            <input type="submit" value="Enviar">
+        </form>
+        <hr>
+        <form name="formulario_dividir" action="Dvision.php" method="POST">
+            <h2>Dividir</h2>
+            <label>Numero 1</label>
+            <input type="number" name="num1">
+            <label>Numero 2</label>
+            <input type="number" name="num2">
+            <input type="submit" value="Enviar">
+        </form>
+        <hr>
+        <form name="formulario_multiplicar" action="Multiplicacion.php" method="POST">
+            <h2>Multiplicar</h2>
+            <label>Numero 1</label>
+            <input type="number" name="num1">
+            <label>Numero 2</label>
+            <input type="number" name="num2">
+            <input type="submit" value="Enviar">
+        </form>
+        <hr>
     </body>
 </html>
